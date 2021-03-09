a5921b4c3ac43d07734344fe443d3d367d5ea270
Bernadette92 <79193891+Bernadette92@users.noreply.github.com>
2021-03-09 17:12:26 +0100

    Add files via upload

---
+++ 19_Eerste JavaScript/index.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+let name = "Bernadette";
+console.log("Bernadette");
+let plus = 4 + 4;
+console.log(4 + 4);
+let multi = 4 * 4;
+console.log(4 * 4);
+let min = 4 - 4;
+console.log(4 - 2);
+let div = 8 / 2;
+console.log(8 / 2);
+let per = 8 % 2;
+console.log(8 % 2);
+
+let age = "twentyEight";
+console.log("twentyEight");
+
+
+
+
